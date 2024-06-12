972e730e3b4bc87ea0666a802e09feee580241ba
shiheng <1761388029@qq.com>
2024-06-12 18:58:59 +0800

    shh commit

--- shh/modules/cloudsim/src/main/java/org/cloudbus/cloudsim/datacenter/la/LAScheduler.java
+++ shh/modules/cloudsim/src/main/java/org/cloudbus/cloudsim/datacenter/la/LAScheduler.java
@@ -8,10 +8,13 @@ import org.cloudbus.cloudsim.Vm;
 import java.util.List;
 
 public class LAScheduler extends Scheduler {
+
     private LAAlgorithm laAlgorithm;
 
     public LAScheduler(List<Cloudlet> cloudletList, List<Vm> vmList) {
+
         super(cloudletList, vmList);
+
         this.laAlgorithm= new LAAlgorithm();
 
         Log.printLine("Using Learning Automata Scheduler");
